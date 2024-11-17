0fe3c5b9f12b124007ad6a17a86993a592363168
GetnetAm <getnetamsalu2119@gmail.com>
2024-11-17 03:09:11 -0800

    configruation e-store computer shopping

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,164 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
+import { useDispatch, useSelector } from "react-redux";
+import SigninScreen from "./screens/SigninScreen";
+import { signout } from "./Redux/actions/userActions";
+import RegisterScreen from "./screens/RegisterScreen";
+import ShippingAddressScreen from "./screens/ShippingAddressScreen";
+import PaymentMethodScreen from "./screens/PaymentMethodScreen";
+import PlaceOrderScreen from "./screens/PlaceOrderScreen";
+import AdminDashboard from "./coponents/admin/AdminDashboard";
+import OrderScreen from "./screens/OrderScreen";
+import OrderHistoryScreen from "./screens/OrderHistoryScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+import PriveateRoute from "./coponents/PriveateRoute";
+import ProtectedRoute from "./coponents/PriveateRoute";
+import Footer from "./coponents/footer/Footer";
+import HeroScreen from "./screens/HeroScreen";
+import { FaCartArrowDown } from "react-icons/fa6";
 
 function App() {
+  const cart = useSelector((state) => state.cart);
+  const { cartItems } = cart;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch();
+
+  const singoutHandler = () => {
+    dispatch(signout());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <ToastContainer />
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <Link to="/" className="brand">
+              Getware CompStore  
+            </Link>
+          </div>
+          <div>
+           
+            <Link to="/home" className="shopping">
+            <FaCartArrowDown className="shopping_icon" />
+             Shopping</Link>
+          </div>
+          <div>
+            <Link to="/cart">
+              Cart
+              {cartItems.length > 0 && (
+                <span className="badge">{cartItems.length}</span>
+              )}
+            </Link>
+            {userInfo ? (
+              <div className="dropdown">
+                <Link to="#">
+                  {userInfo.name} <i class="fa-solid fa-caret-down"></i>
+                </Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/orderhistory">Order History</Link>
+                  </li>
+                  <li>
+                    <Link to="/profile">User Details</Link>
+                  </li>
+                  <li>
+                    <Link to="#signout" onClick={singoutHandler}>
+                      Sign Out
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            ) : (
+              <Link to="/signin">Sign In</Link>
+            )}
+          </div>
+
+          {
+            userInfo && userInfo.isAdmin &&(
+              <div className="dropdown">
+                <Link to="#admin">Admin <i class="fa-solid fa-caret-down"></i></Link>
+                <ul className="dropdown-content">
+                  <li>
+                    <Link to="/dashboard">Dashboard</Link>
+                  </li><li>
+                    <Link to="/dashboard">Products</Link>
+                  </li>
+                  <li>
+                    <Link to="/dashboard">User</Link>
+                  </li>
+
+
+                </ul>
+              </div>
+            )
+          }
+        </header>
+
+        <main>
+          {/* {userInfo && userInfo.isAdmin===true ?
+            (
+              <Routes>
+                 <Route path="/admin" element={<AdminDashboard />} />
+              </Routes>
+            ):
+            ( */}
+
+          {/* {
+                 userInfo && userInfo.isAdmin?(
+                    <Routes>
+                       <Route path="/" element={<AdminDashboard />} />
+
+                    </Routes>
+                    
+
+                  ):
+                  (
+                  ) } */}
+          <Routes>
+          <Route path="/" element={<HeroScreen />}  />
+            <Route path="/home" element={<HomeScreen />} />
+            <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
+            <Route path="/signin" element={<SigninScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
+            <Route path="/shipping" element={<ShippingAddressScreen />} />
+            <Route path="/payment" element={<PaymentMethodScreen />} />
+            <Route path="/placeorder" element={<PlaceOrderScreen />} />
+            <Route path="/order/:id" element={<OrderScreen />} />
+            <Route path="/orderhistory" element={<OrderHistoryScreen />} />
+            {/* <PriveateRoute path="/profile" element={<ProfileScreen/>} /> */}
+
+            <Route
+              path="/profile"
+              element={
+                <ProtectedRoute>
+                  <ProfileScreen />
+                </ProtectedRoute>
+              }
+            />
+          </Routes>
+          
+          
+         
+              
+
+     
+        </main>
+
+        {/* <footer className="row center"> */}
+          <Footer />
+{/* 
+        </footer> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/data.js
@@ -0,0 +1,261 @@
+const data= {
+    products: [
+      {
+        _id: '1',
+        name: 'Hp Desctop',
+        category: 'Desktop',
+        image: '/images/c1.jpeg',
+        price: 460,
+        brand: 'Hp',
+        rating: 4.5,
+        countInStock:10,
+        numReviews: 10,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '2',
+        name: 'Hp laptop',
+        category: 'laptop',
+        image: '/images/c3.jpeg',
+        price: 350,
+        brand: ' Hp',
+        rating: 4.2,
+        countInStock:5,
+        numReviews: 5,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '3',
+        name: 'Desktop',
+        category: 'Desktop',
+        image: '/images/c2.jpeg',
+        price: 270,
+        brand: ' lenvo',
+        rating: 4.5,
+        countInStock:2,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      }, {
+        _id: '4',
+        name: 'Toshiba Pc',
+        category: 'laptop',
+        image: '/images/c4.jpeg',
+        price: 170,
+        brand: ' toshiba',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '5',
+        name: 'Hp Desctop',
+        category: 'Desktop',
+        image: '/images/d3.jpeg',
+        price: 460,
+        brand: 'Hp',
+        rating: 4.5,
+        countInStock:10,
+        numReviews: 10,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '6',
+        name: 'Hp laptop',
+        category: 'laptop',
+        image: '/images/c5.jpeg',
+        price: 350,
+        brand: ' Hp',
+        rating: 4.2,
+        countInStock:5,
+        numReviews: 5,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '7',
+        name: 'Desktop',
+        category: 'Desktop',
+        image: '/images/d5.jpeg',
+        price: 270,
+        brand: ' lenvo',
+        rating: 4.5,
+        countInStock:2,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      }, {
+        _id: '8',
+        name: 'Toshiba Pc',
+        category: 'laptop',
+        image: '/images/c6.jpeg',
+        price: 170,
+        brand: ' toshiba',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '9',
+        name: 'Hp Desctop',
+        category: 'Desktop',
+        image: '/images/d7.jpeg',
+        price: 460,
+        brand: 'Hp',
+        rating: 4.5,
+        countInStock:10,
+        numReviews: 10,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '10',
+        name: 'Hp laptop',
+        category: 'laptop',
+        image: '/images/c7.jpeg',
+        price: 350,
+        brand: ' Hp',
+        rating: 4.2,
+        countInStock:5,
+        numReviews: 5,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '11',
+        name: 'Desktop',
+        category: 'Desktop',
+        image: '/images/d8.jpeg',
+        price: 270,
+        brand: ' lenvo',
+        rating: 4.5,
+        countInStock:2,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      }, {
+        _id: '12',
+        name: 'Toshiba Pc',
+        category: 'laptop',
+        image: '/images/c9.jpeg',
+        price: 170,
+        brand: ' toshiba',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+
+      {
+        _id: '13',
+        name: 'Apple Pc',
+        category: 'laptop',
+        image: '/images/c15.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+
+      {
+        _id: '14',
+        name: 'apple Pc',
+        category: 'laptop',
+        image: '/images/c16.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '15',
+        name: 'apple Pc',
+        category: 'laptop',
+        image: '/images/c17.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '16',
+        name: 'apple Pc',
+        category: 'laptop',
+        image: '/images/c18.jpeg',
+        price: 170,
+        brand: ' toshiba',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '17',
+        name: 'Apple Pc...',
+        category: 'apple',
+        image: '/images/c26.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '18',
+        name: 'Hp Pc',
+        category: 'laptop',
+        image: '/images/c20.jpeg',
+        price: 170,
+        brand: ' hp',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '19',
+        name: 'apple',
+        category: 'laptop',
+        image: '/images/c21.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+      {
+        _id: '20',
+        name: 'Apple Pc',
+        category: 'laptop',
+        image: '/images/c22.jpeg',
+        price: 170,
+        brand: ' apple',
+        rating: 4.5,
+        countInStock:0,
+        numReviews: 8,
+        description:"core i 9 gen 10 ssd 1 tra"
+      },
+    ]
+  };
+  export default data;
+
+  export const laptop = [
+    {
+      id: 1,
+      name: "admin dashboard",
+      image: "../images/portfolio1/p11.JPG",
+      title: "Portfolio 1",
+      description: "health insurance project Design with Mern stack",
+    },
+    {
+      id: 2,
+  
+      image: "../images/portfolio1/p12.JPG",
+  
+  
+    },
+  ];
